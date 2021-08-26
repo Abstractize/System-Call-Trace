acd34018301c986ec2d8cf447d73601a01587847
MariaAvila <mariafer50121@gmail.com>
2021-08-25 21:24:57 -0700

    [add] Identification of mode and filter

--- src/main.c
+++ src/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
-int main()
+#include <string.h>
+
+char *get_from_argv(char *compare, int argc, char *argv[])
 {
-    printf("Hello, World!\n");
-    return 0;
+    int counter;
+    for (counter = 0; counter < argc; counter++)
+    {
+        if (!strcmp(compare, argv[counter]))
+            return argv[counter + 1];
+    }
+    return "";
+}
+
+int main(int argc, char *argv[])
+{
+    char *mode = get_from_argv("--mode", argc, argv);
+    char *filter = get_from_argv("--filter", argc, argv);
+    printf("Set mode is %s\n", mode);
+    printf("Set filter is %s\n", filter);
 }
